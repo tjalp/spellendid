28de78fd8b992a115e743df108ee55af42100845
tjalp <tiesalkemade@gmail.com>
2022-04-26 19:52:43 +0200

    feat: show toast when joining smash wizards

--- fabric/src/main/kotlin/net/tjalp/spellendid/fabric/networking/FabricNetworkHandler.kt
+++ fabric/src/main/kotlin/net/tjalp/spellendid/fabric/networking/FabricNetworkHandler.kt
@@ -2,11 +2,16 @@ package net.tjalp.spellendid.fabric.networking
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.toast.SystemToast
+import net.minecraft.text.TranslatableText
 import net.minecraft.util.Identifier
 import net.tjalp.spellendid.core.Spellendid
 import net.tjalp.spellendid.core.networking.HANDSHAKE
 import net.tjalp.spellendid.core.networking.NetworkHandler
+import net.tjalp.spellendid.core.util.EXECUTOR_SERVICE
 import net.tjalp.spellendid.fabric.SpellendidFabric
+import java.util.concurrent.TimeUnit
 
 class FabricNetworkHandler : NetworkHandler<Identifier>() {
 
@@ -36,6 +41,17 @@ class FabricNetworkHandler : NetworkHandler<Identifier>() {
     override fun connect() {
         Spellendid.LOGGER.warn("Connected to Smash Wizards!")
         super.connect()
+
+        EXECUTOR_SERVICE.schedule({
+            if (connected) {
+                SystemToast.add(
+                    MinecraftClient.getInstance().toastManager,
+                    SystemToast.Type.TUTORIAL_HINT,
+                    TranslatableText("toast.spellendid.connected.line1"),
+                    TranslatableText("toast.spellendid.connected.line2"),
+                )
+            }
+        }, 2L, TimeUnit.SECONDS)
     }
 
     override fun disconnect() {
